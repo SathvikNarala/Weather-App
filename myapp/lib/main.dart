497b50a4881933eebe4d43c4075bcfc64ded786d
Sathvik Narala <sathviknarala2003@gmail.com>
2023-07-05 12:00:23 +0530

    Bloc Creation

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'clima.dart';
 
-void main() => runApp(const MyApp());
+void main(){
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
